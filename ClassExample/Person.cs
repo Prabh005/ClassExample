50b52d6d468ac9377784df967ed7658e99f5aeba
PG005 <Prabhneetgrewal9@gmail.com>
2021-04-09 12:13:48 +1200

    base code

---
+++ ClassExample/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassExample
+{
+    class Person
+    {
+        // Member Variables
+        // method s
+
+        private  string name { get;  set; }
+        private int age { get; set; }
+
+        // Methods
+
+        public void SetMember (string setName, int setsalary, sting setDeparment )
+        {
+            name = setName;
+            Salary = setsarly;
+            department = setDeparment; 
+        }
+
+        public void GetMember(out string getName, out int getAge)
+        {
+            getName =name;
+            getAge = age;
+        }
+
+
+    }
+
+    
+}
